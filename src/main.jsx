5fb3148626314d246bb9b0f8beb58b649f3a774b
allysonbelo <allysonbelo@gmail.com>
2023-11-27 14:04:18 -0300

    Refact: Code refacted

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './pages/App.jsx'
+
+import GlobalStyles from './styles'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
 )
---
+++ src/pages/App.jsx
@@ -0,0 +1,12 @@
+import gitLogo from '../assets/githublogowhite.jpg'
+import {Container} from './styles'
+
+function App() {
+  return (
+    <Container>
+      <img src={gitLogo} width={72} height={72} alt="Logo github created by IA" />
+    </Container>
+  )
+}
+
+export default App
