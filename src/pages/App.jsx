6be186fb88fa4cc570eca29665a9b7cac4b95629
allysonbelo <allysonbelo@gmail.com>
2023-11-27 19:09:50 -0300

    Add: Input component done

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,10 +1,12 @@
 import gitLogo from '../assets/githublogowhite.jpg'
+import Input from '../components/input'
 import {Container} from './styles'
 
 function App() {
   return (
     <Container>
       <img src={gitLogo} width={72} height={72} alt="Logo github created by IA" />
+      <Input />
     </Container>
   )
 }
